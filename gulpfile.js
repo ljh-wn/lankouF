89531b60474104cd63f8f9bb2d00c05f52dea363
ljh-wn <2974328623@qq.com>
2019-10-15 23:00:24 +0800

    实现注册验证功能

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,37 @@ const gulp=require("gulp");
 // 获取gulp下的sass
 const sass=require("gulp-sass");
 //获取gulp下的压缩文件
-const minfyCSS=require("gulp-minify-css");
+
 // 将所有的html,css,js都要拷过去（copy）
 gulp.task("copy", async ()=>{
 	gulp.src("./**/*")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\lankou"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\lankouF"));
 })
 
 //任务监听
 gulp.task("watchall", async ()=>{
 	gulp.watch("*.html", async ()=>{
        gulp.src("*.html")
-       .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankou"));
+       .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankouF"));
 	});
 	//监听sass下的所有文件
 	gulp.watch("lankouSass/**/*", async ()=>{
 		gulp.src("lankouSass/**/*")
 		.pipe(sass())
-		.pipe(minfyCSS())
-        .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankou\\css"));
+		
+        .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankouF\\css"));
 	});
 	gulp.watch("img/*.{jpg,png}",async ()=>{
 		gulp.src("img/*.{jpg,png}")
-	   .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankou\\img"));
+	   .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankouF\\img"));
 	});
 	gulp.watch("js/*.js", async ()=>{
-       gulp.src("*.js")
-       .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankou\\js"));
+       gulp.src("js/*.js")
+       .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankouF\\js"));
+	});
+	gulp.watch("css/*.css", async ()=>{
+	   gulp.src("css/*.css")
+	   .pipe(gulp.dest("D:\\phpStudy\\WWW\\lankouF\\css"));
 	});
 	
 
---
+++ js/random.js
@@ -0,0 +1,8 @@
+function getMa(obj){
+	let Num=parseInt(Math.random()*10000)
+    while(Num<1000){
+	    Num=parseInt(Math.random()*10000)
+	   }
+	   return Num;
+	   
+}
